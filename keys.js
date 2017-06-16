336022cf41512a38fb210bee996e0ebd59118de2
emilypo89 <emilypo89@gmail.com>
2017-06-15 20:40:41 -0400

    set up

---
+++ keys.js
@@ -0,0 +1,9 @@
+// ```JavaScript
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '72g1VaZyWCSAhFjw4qnBUZvjY',
+  consumer_secret: 'O9f3eU8AV39z53XvNMXDXZZCUhlZWC2Yt666GLyliqLvvgpufu',
+  access_token_key: '875508489547808769-9ZCP2SzaWMkXh8WEmQ8VUxkJiJXLoZ3',
+  access_token_secret: 'ytx0AAjTgB01nc7g6zdSDN5yD9YsDA0iHPvhsioi1HIlm',
+}
