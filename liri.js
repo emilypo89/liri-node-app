04b17196a08ec9c0c6f704e39a5b9815c033744e
emilypo89 <emilypo89@gmail.com>
2017-06-17 13:59:54 -0400

    working spotify section

--- liri.js
+++ liri.js
@@ -9,9 +9,9 @@ switch (action) {
     readTweets();
     break;
 
-  // case "spotify-this-song":
-  //   spotifySong();
-  //   break;
+  case "spotify-this-song":
+    spotifySong();
+    break;
 
   case "movie-this":
     movie();
@@ -42,7 +42,6 @@ function readTweets(){
     if (!error) {
       console.log(tweets);
     }
-
     else {
       console.log(error);
     }
@@ -50,6 +49,52 @@ function readTweets(){
 }
 
 //    * `spotify-this-song`
+function spotifySong () {
+  var Spotify = require('node-spotify-api');
+ 
+  var spotify = new Spotify({
+    id: '6c24d65f73f74616891a1d7f69eb92e8',
+    secret: '8b40ae994ff544099fa1eeda48ae7bce'
+  });
+  var nodeArgs = process.argv;
+  var songSearch = "";
+
+  // pulls the user input and adds a + between the words
+  for (var i = 3; i < nodeArgs.length; i++) {
+
+    if (i > 3 && i < nodeArgs.length) {
+      songSearch = songSearch + " " + nodeArgs[i];
+    }
+
+    else {
+      songSearch += nodeArgs[i];
+    }
+  }
+  // song search if there is no user input
+  if (songSearch == ""){
+    spotify.search({ type: 'track', query: 'I Saw the Sign', limit: 1}, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+      console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
+      console.log("Song Title: " + data.tracks.items[0].name);
+      console.log("Album: " + data.tracks.items[0].album.name);
+      console.log("Preview Link: " + data.tracks.items[0].preview_url);
+    });
+  }
+  // song search of user input
+  else {
+    spotify.search({ type: 'track', query: songSearch, limit: 1}, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+      console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
+      console.log("Song Title: " + data.tracks.items[0].name);
+      console.log("Album: " + data.tracks.items[0].album.name);
+      console.log("Preview Link: " + data.tracks.items[0].preview_url);
+    });
+  }
+}
 
 // * `movie-this`
 function movie(){
@@ -82,7 +127,7 @@ function movie(){
         console.log("Language: " + JSON.parse(body).Language);
         console.log("Plot: " + JSON.parse(body).Plot);
         console.log("Actors Featured: " + JSON.parse(body).Actors);
-        // console.log("Link to Rotten Tomatoes: " + JSON.parse(body).Ratings[1]);
+        console.log(JSON.parse(body).Ratings[1]);
       }
     });
   }
@@ -97,7 +142,7 @@ function movie(){
         console.log("Language: " + JSON.parse(body).Language);
         console.log("Plot: " + JSON.parse(body).Plot);
         console.log("Actors Featured: " + JSON.parse(body).Actors);
-        // console.log("Link to Rotten Tomatoes: " + JSON.parse(body).Ratings[1]);
+        console.log(JSON.parse(body).Ratings[1]);
       }
     });
   }
